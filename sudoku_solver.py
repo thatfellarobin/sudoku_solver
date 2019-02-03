6a57e66f19781d57b9e1d956bd75420de9b6641d
Robin Liu <Robin.Liu.831@gmail.com>
2019-02-03 14:49:19 -0500

    python 3 update

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,19 +1,22 @@
 import csv
 import sys
 
+
 # rule checks return True if it breaks the rule
-def rowrule(index, puzzle): # puzzle is the "solution" array
+def rowrule(index, puzzle):  # puzzle is the "solution" array
     for e in range(index - (index % 9), index - (index % 9) + 9):
         if puzzle[index] == puzzle[e] and index != e:
             return True
     return False
 
+
 def colrule(index, puzzle):
     column = index % 9
     for e in range(9):
-        if puzzle[index] == puzzle[e*9 + column] and index != e*9 + column:
+        if puzzle[index] == puzzle[e * 9 + column] and index != e * 9 + column:
             return True
     return False
+
 
 def blockrule(index, puzzle):
     blockrowindex = index // 27
@@ -24,13 +27,15 @@ def blockrule(index, puzzle):
                 return True
     return False
 
-def backtrack(index, givenvalues): # givenvalues is the givens array
+
+def backtrack(index, givenvalues):  # givenvalues is the givens array
     index -= 1
     while givenvalues[index]:
         index -= 1
         if index < 0:
             sys.exit("puzzle impossible")
     return index
+
 
 # Initialize relevant lists
 sudoku = [0] * 81
@@ -42,28 +47,28 @@ with open('sudoku.csv', "rb") as fin:
     sudokuraw = list(reader)
 for r in range(9):
     for c in range(9):
-        sudoku[r*9 + c] = int(sudokuraw[r][c])
-        if sudoku[r*9 + c] != 0:
-            givens[r*9 + c] = True
+        sudoku[r * 9 + c] = int(sudokuraw[r][c])
+        if sudoku[r * 9 + c] != 0:
+            givens[r * 9 + c] = True
 
 # The solve loop
 ind = 0
 while ind < 81:
-    if not givens[ind]: # if that square wasn't part of the given puzzle
+    if not givens[ind]:  # if that square wasn't part of the given puzzle
         sudoku[ind] += 1
 
-        if sudoku[ind] >= 10: # if it cannot be increased
+        if sudoku[ind] >= 10:  # if it cannot be increased
             sudoku[ind] = 0
             ind = backtrack(ind, givens)
 
-        elif not (rowrule(ind, sudoku) or colrule(ind, sudoku) or blockrule(ind, sudoku)): # if it doesn't violate any rules
-            ind += 1 # move on to the next square
+        elif not (rowrule(ind, sudoku) or colrule(ind, sudoku) or blockrule(ind, sudoku)):  # if it doesn't violate any rules
+            ind += 1  # move on to the next square
 
-        else: # if it breaks a rule but isn't at 9 yet
-            pass # do nothing (number increments on next loop)
+        else:  # if it breaks a rule but isn't at 9 yet
+            pass  # do nothing (number increments on next loop)
 
-    else: # otherwise that square was part of the given puzzle
-        ind += 1 # move on to the next square
+    else:  # otherwise that square was part of the given puzzle
+        ind += 1  # move on to the next square
 
 # Output solved puzzle
 solved = [[0, 0, 0, 0, 0, 0, 0, 0, 0]] * 9
@@ -71,8 +76,8 @@ fout = open('sudoku_solved.csv', "wb")
 writer = csv.writer(fout)
 for r in range(9):
     for c in range(9):
-        solved[r][c] = sudoku[r*9 + c]
-    print solved[r] # show solution to screen
+        solved[r][c] = sudoku[r * 9 + c]
+    print(solved[r])  # show solution to screen
     # writer.writerow(solved[r]) # save solution to file
 
 fin.close()
